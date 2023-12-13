4703da630a9399ed7fbe8a3395a313d9de65c2d0
nathantye <tyenathan@yahoo.co.uk>
2023-12-13 16:25:09 +0000

    removed bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./styles.css";
 import mecuryImage from "./assets/images/planet-mercury.svg";
+import navBarImage from "./assets/images/icon-hamburger.svg";
 import { useState, useEffect } from "react";
+import { isBrowser, isMobile } from "react-device-detect";
 
 function App() {
   const [data, setData] = useState(null);
@@ -37,111 +39,60 @@ function App() {
       {loading && <p>Loading...</p>}
       {error && <p>Error: {error.message}</p>}
       {!loading && !error && (
-        <div className="container-fluid">
-          <div className="row row-cols-2 row-cols-md-1 ">
-            <div className="col d-flex justify-content-md-center">
-              <div className="planets-header">
-                <h2>THE PLANETS</h2>
-              </div>
+        <div>
+          <h2>THE PLANETS</h2>
+          <img src={navBarImage} />
+          <div id="is-mobile">
+            <div className="">
+              <h3>OVERVIEW</h3>
             </div>
-            <div className="col d-flex justify-content-sm-end justify-content-md-center">
-              <nav className="navbar navbar-expand-md">
-                <div className="container ">
-                  <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="offcanvas"
-                    data-bs-target="#offcanvasNavbar"
-                    aria-controls="offcanvasNavbar"
-                    aria-label="Toggle navigation"
-                  >
-                    <span className="navbar-toggler-icon"></span>
-                  </button>
-                  <div
-                    className="offcanvas offcanvas-end"
-                    tabIndex="-1"
-                    id="offcanvasNavbar"
-                    aria-labelledby="offcanvasNavbarLabel"
-                  >
-                    <div className="offcanvas-header">
-                      <h5 className="offcanvas-title" id="offcanvasNavbarLabel">
-                        Offcanvas
-                      </h5>
-                      <button
-                        type="button"
-                        className="btn-close"
-                        data-bs-dismiss="offcanvas"
-                        aria-label="Close"
-                      ></button>
-                    </div>
-                    <div className="offcanvas-body">
-                      <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                        <li className="nav-item">
-                          <a
-                            className="nav-link active"
-                            aria-current="page"
-                            href="#"
-                          >
-                            {planetData.name}
-                          </a>
-                        </li>
-                        {/* PUT A HREF ATTRIBUTE IN HERE THAT SOMEHOW CHANGES THE PLANETDATA ATTRIBUTE TO A DIFFERENT PLANET THEREFORE RENDERING SOMETHING ELSE */}
-                        {navContent.map((data) => (
-                          <li className="nav-link" key={data}>
-                            {data}
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-              </nav>
+            <div className="">
+              <h3>STRUCTURE</h3>
+            </div>
+            <div className="">
+              <h3>SURFACE</h3>
             </div>
           </div>
 
-          <div className="row row-cols-3" id="is-mobile">
-            <div className="col d-flex justify-content-center">
-              <h4>OVERVIEW</h4>
-            </div>
-            <div className="col d-flex justify-content-center">
-              <h4>STRUCTURE</h4>
-            </div>
-            <div className="col d-flex justify-content-center">
-              <h4>SURFACE</h4>
-            </div>
-          </div>
-
-          <div className="row row-cols-1">
-            <div className="col d-flex justify-content-center ">
+          <div className="">
+            <div id="planet-card">
               <img id="mercury-image" src={mecuryImage} />
             </div>
-            <div className="col d-flex justify-content-center flex-wrap ">
-              <>
-                {" "}
-                <h1>{planetData.name}</h1>
-                <p>{planetData.overview.content}</p>
-                <a href={planetData.overview.source}>Source Wikipedia</a>
-              </>
+            <div>
+              <div>
+                <>
+                  <h1>{planetData.name}</h1>
+
+                  <p>{planetData.overview.content}</p>
+                  <a href={planetData.overview.source}>Source Wikipedia</a>
+                </>
+              </div>
+
+              <div id="is-browser">
+                <div>
+                  <h3>OVERVIEW</h3>
+
+                  <h3>STRUCTURE</h3>
+
+                  <h3>SURFACE</h3>
+                </div>
+              </div>
             </div>
           </div>
 
-          <div className="row row-cols-1">
-            {data && (
-              <>
-                <div className="col d-flex justify-content-between">
-                  <h4>ROTATION TIME</h4> <span>{planetData.rotation}</span>
-                </div>
-                <div className="col d-flex justify-content-between">
-                  <h4>ROTATION TIME</h4> <span>{planetData.revolution}</span>
-                </div>
-                <div className="col d-flex justify-content-between">
-                  <h4>ROTATION TIME</h4> <span>{planetData.radius}</span>
-                </div>
-                <div className="col d-flex justify-content-between">
-                  <h4>ROTATION TIME</h4> <span>{planetData.temperature}</span>
-                </div>
-              </>
-            )}
+          <div className="">
+            <div className="">
+              <h4>ROTATION TIME</h4> <h2>{planetData.rotation}</h2>
+            </div>
+            <div className="">
+              <h4>REVOLUTION TIME</h4> <h2>{planetData.revolution}</h2>
+            </div>
+            <div className="">
+              <h4>RADIUS</h4> <h2>{planetData.radius}</h2>
+            </div>
+            <div className="">
+              <h4>AVERAGE TEMP.</h4> <h2>{planetData.temperature}</h2>
+            </div>
           </div>
         </div>
       )}
